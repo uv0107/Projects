6ece041abb70f190463307500aabd9b8973f5c41
uv0107 <uyyalavamsi37@gmail.com>
2025-04-20 01:13:47 +0530

    adding app.js code

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Results from './Results';
+import Voting from './VotingB';
+import AdminPanel from './AdminPanelB';
+import OpenB from './OpenB';
+// import MetaMask from './MetaMaskConnection';
+// import { Meta } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<OpenB/>} />
+        <Route path='/AdminPanel' element={<AdminPanel/>} />
+        <Route path='/Voting' element={<Voting />} />
+        <Route path='/Results' element={<Results/>} />
+      </Routes>
+  </BrowserRouter>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
